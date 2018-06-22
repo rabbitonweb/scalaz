0397693907fe78c28188a0b4140d479e5946f165
Pawel Szulc <paul.szulc@gmail.com>
2018-06-23 00:12:48 +0200

    Add ApplicativeAsk

---
+++ base/shared/src/main/scala/scalaz/mtl/ApplicativeAsk.scala
@@ -0,0 +1,23 @@
+package scalaz.mtl
+
+import scalaz.ct.ApplicativeClass
+
+trait ApplicativeAsk[F[_], E] {
+  val applicative: ApplicativeClass[F]
+
+  def ask: F[E]
+
+  def reader[A](f: E => A): F[A]
+}
+
+trait DefaultApplicativeAsk[F[_], E] extends ApplicativeAsk[F, E]  {
+  def reader[A](f: E => A): F[A] = applicative.map(ask)(f)
+}
+
+object ApplicativeAsk {
+  def apply[F[_], E](implicit ev: ApplicativeAsk[F, E]): ApplicativeAsk[F, E] = ev
+
+  def ask[F[_], E](implicit ev: ApplicativeAsk[F, E]): F[E] = ev.ask
+
+  def reader[F[_], E, A](f: E => A)(implicit ev: ApplicativeAsk[F, E]): F[A] = ev.reader(f)
+}
